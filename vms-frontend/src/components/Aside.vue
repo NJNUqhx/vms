8dfc5572945455b73ff8439667d07ad4f46fffd7
NJNUqhx <3380595029@qq.com>
2024-11-15 14:33:43 +0800

    实现导航栏伸缩

--- vms-frontend/src/components/Aside.vue
+++ vms-frontend/src/components/Aside.vue
@@ -10,17 +10,17 @@
 
     <el-menu-item index="/Home">
       <i class="el-icon-s-home"></i>
-      <span>首页</span>
+      <span slot="title">首页</span>
     </el-menu-item>
 
     <el-menu-item index="/One">
       <i class="el-icon-s-flag"></i>
-      <span>导航一</span>
+      <span slot="title">导航一</span>
     </el-menu-item>
 
     <el-menu-item index="/Two">
       <i class="el-icon-s-opportunity"></i>
-      <span>导航二</span>
+      <span slot="title">导航二</span>
     </el-menu-item>
   </el-menu>
 </template>
@@ -28,10 +28,8 @@
 <script>
 export default {
   name: "Aside",
-  data(){
-    return {
-      isCollapse: false
-    }
+  props:{
+    isCollapse: Boolean
   }
 }
 </script>
